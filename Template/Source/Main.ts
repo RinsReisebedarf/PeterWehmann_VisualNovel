d4014cbc93bd779a753ee509397d6f5d5f615990
RinsReisebedarf <rins.reisebedarf@gmail.com>
2022-04-18 11:08:02 +0200

    FirstPictures

--- Template/Source/Main.ts
+++ Template/Source/Main.ts
@@ -4,6 +4,67 @@ namespace Template {
 
   console.log("FudgeStory template starting");
 
+// define transitions
+export let transitions = {
+  puzzle {
+    duration: 1,
+    alpha "./Tutorial_WS21/FreeTransitions/JigsawThemedTransitions/puzzle.png",
+    edge: 1
+  }
+}
+
+export let sound = {
+  // themes
+  nightclub: "./Tutorial_WS21/Audio/Nightclub.ogg",
+
+  // SFX
+  click: "Pfad"
+};
+
+export let locations = {
+  nightpark: {
+    name: "",
+    background: "./Tutorial_WS21/Images/Background/Nightpark.png"
+  },
+  starry: {
+    name: "Starry"
+    background: "Pfad"
+  }
+};
+name: ""
+
+export let characters = {
+  narrator: {
+    name: ""
+  },
+  aisaka: {
+    name: "Aisaka"
+    origin: ƒS.ORIGIN.BOTTOMCENTER,
+    pose: {
+      angry: "Pfad",
+      happy: "",
+      upset: ""
+    }
+  },
+  kohanna: {
+    name: "Kohanna",
+    origin: ƒS.ORIGIN.BOTTOMCENTER,
+    Pose: {
+      angry: ""
+      happy: ""
+      upset: ""
+    }
+  }
+};
+
+export let DataForSave = {
+  nameProtagonist: ""
+  score: 0
+};
+
+
+
+
   window.addEventListener("load", start);
   function start(_event: Event): void {
     let scenes: ƒS.Scenes = [
--- Template/Source/Scene.ts
+++ Template/Source/Scene.ts
@@ -1,5 +1,21 @@
 namespace Template {
   export async function Scene(): ƒS.SceneReturn {
     console.log("FudgeStory Template Scene starting");
+
+    let text = {
+      Narrator: {
+        T0001: ""
+        T0002: ""
+      },
+      Aisaka: {
+        T0001: ""
+        T0002:
+      }
+    };
+
+    await ƒS.Speech.tell(characters.aisaka, text.Aisaka.T0001);
+    await ƒS.Speech.tell(characters.aisaka, text.Aisaka.T0002);
+    await ƒS.Speech.tell(characters.aisaka, "Neuer Text");
+
   }
 }
